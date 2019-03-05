d4438d740ba1ef0b184663cfa8890d69512b8ff7
Filip Szczesniak <fiszczu@gmail.com>
2019-03-05 23:45:27 +0100

    set configuration for jest testing

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "roots": [
+        "."
+    ],
+    "transform": {
+        "^.+\\.ts?$": "ts-jest"
+    },
+    "testRegex": "(/__test__/.*|(\\.|/)(test|spec))\\.ts?$",
+    "moduleFileExtensions": [
+        "ts",
+        "js",
+        "json",
+        "node",
+    ],
+};
